e98cb5c3e4d8235158f33155885417b1ebed49dd
mohdanas86 <anasalam7739@gmail.com>
2025-04-12 16:45:06 +0530

    docker and requirement

--- context/MyContext.js
+++ context/MyContext.js
@@ -186,7 +186,7 @@ export function MyContextProvider({ children }) {
       .filter(Boolean); // remove nulls
 
     try {
-      const url = "http://127.0.0.1:5000/bulk_predict";
+      const url = `${process.env.CATOGERY_MODE}/bulk_predict`;
       const response = await axios.post(url, {
         transactions: formatted,
       });
