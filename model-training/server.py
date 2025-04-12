70636a5e42212b833dc8a9d814fb681df9917c84
mohdanas86 <anasalam7739@gmail.com>
2025-04-12 16:08:34 +0530

    8080

--- model-training/server.py
+++ model-training/server.py
@@ -84,4 +84,4 @@ def root():
     return jsonify({"message": "Bank Transaction Categorizer is running."})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
